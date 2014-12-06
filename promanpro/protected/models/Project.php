6f67ad9a912d963467c74dbe1fce663947ac7e29
yusufcahyo <yusuf.cahyo@ui.ac.id>
2014-12-06 16:21:35 +0700

    add validation for username, name, password and date on Feature Project

--- promanpro/protected/models/Project.php
+++ promanpro/protected/models/Project.php
@@ -12,71 +12,98 @@
  * @property string $End_Date
  * @property string $adminProject
  */
-class Project extends CActiveRecord
-{
+class Project extends CActiveRecord {
 	/**
+	 *
 	 * @return string the associated database table name
 	 */
-	public function tableName()
-	{
+	public function tableName() {
 		return 'project';
 	}
-
+	
 	/**
+	 *
 	 * @return array validation rules for model attributes.
 	 */
-	public function rules()
-	{
+	public function rules() {
 		// NOTE: you should only define rules for those attributes that
 		// will receive user inputs.
-		return array(
-			array('Name,Start_Date', 'required'),
-                        array('Name', 'unique'),
-			array('Name', 'length', 'max'=>20),
-			array('Status', 'length', 'max'=>10),
-			array('adminProject', 'length', 'max'=>30),
-			array('Description, End_Date', 'safe'),
-                        array('End_Time', 'cekWaktu'),
-			// The following rule is used by search().
-			// @todo Please remove those attributes that should not be searched.
-			array('ID, Name, Description, Status, Start_Date, End_Date, adminProject', 'safe', 'on'=>'search'),
+		return array (
+				array (
+						'Name,Start_Date',
+						'required',
+						'pattern' => '/[^(19|20)\d\d[\-\/.](0[1-9]|1[012])[\-\/.](0[1-9]|[12][0-9]|3[01])]/'
+				),
+				array (
+						'Name',
+						'unique' 
+				),
+				array (
+						'Name',
+						'length',
+						'max' => 20 
+				),
+				array (
+						'Status',
+						'length',
+						'max' => 10 
+				),
+				array (
+						'adminProject',
+						'length',
+						'max' => 30 
+				),
+				array (
+						'Description, End_Date',
+						'safe' 
+				),
+				array (
+						'End_Time',
+						'cekWaktu',
+						'pattern' => '/[^(19|20)\d\d[\-\/.](0[1-9]|1[012])[\-\/.](0[1-9]|[12][0-9]|3[01])]/'
+				),
+				// The following rule is used by search().
+				// @todo Please remove those attributes that should not be searched.
+				array (
+						'ID, Name, Description, Status, Start_Date, End_Date, adminProject',
+						'safe',
+						'on' => 'search' 
+				) 
 		);
 	}
-        
-        public function cekWaktu()
-        {
-            if($this->Start_Date > $this->End_Date){
-                $this->addError('End_Date', 'The Start Date cannot be older than End Date') ;
-            }
-        }
-
+	public function cekWaktu() {
+		if ($this->Start_Date > $this->End_Date) {
+			$this->addError ( 'End_Date', 'The Start Date cannot be older than End Date' );
+		}
+	}
+	
+	
 	/**
+	 *
 	 * @return array relational rules.
 	 */
-	public function relations()
-	{
+	public function relations() {
 		// NOTE: you may need to adjust the relation name and the related
 		// class name for the relations automatically generated below.
-		return array(
-		);
+		return array ();
 	}
-
+	
 	/**
+	 *
 	 * @return array customized attribute labels (name=>label)
 	 */
-	public function attributeLabels()
-	{
-		return array(
-			'ID' => 'ID',
-			'Name' => 'Name',
-			'Description' => 'Description',
-			'Status' => 'Status',
-			'Start_Date' => 'Start Date',
-			'End_Date' => 'End Date',
-			'adminProject' => 'Admin Project',
+	public function attributeLabels() {
+		return array (
+				'ID' => 'ID',
+				'Name' => 'Name',
+				'Description' => 'Description',
+				'Status' => 'Status',
+				'Start_Date' => 'Start Date',
+				'End_Date' => 'End Date',
+				'adminProject' => 'Admin Project' 
 		);
 	}
-
+	
 	/**
 	 * Retrieves a list of models based on the current search/filter conditions.
 	 *
@@ -87,35 +114,34 @@ class Project extends CActiveRecord
 	 * - Pass data provider to CGridView, CListView or any similar widget.
 	 *
 	 * @return CActiveDataProvider the data provider that can return the models
-	 * based on the search/filter conditions.
+	 *         based on the search/filter conditions.
 	 */
-	public function search()
-	{
+	public function search() {
 		// @todo Please modify the following code to remove attributes that should not be searched.
-
-		$criteria=new CDbCriteria;
-
-		$criteria->compare('ID',$this->ID);
-		$criteria->compare('Name',$this->Name,true);
-		$criteria->compare('Description',$this->Description,true);
-		$criteria->compare('Status',$this->Status,true);
-		$criteria->compare('Start_Date',$this->Start_Date,true);
-		$criteria->compare('End_Date',$this->End_Date,true);
-		$criteria->compare('adminProject',$this->adminProject,true);
-
-		return new CActiveDataProvider($this, array(
-			'criteria'=>$criteria,
-		));
+		$criteria = new CDbCriteria ();
+		
+		$criteria->compare ( 'ID', $this->ID );
+		$criteria->compare ( 'Name', $this->Name, true );
+		$criteria->compare ( 'Description', $this->Description, true );
+		$criteria->compare ( 'Status', $this->Status, true );
+		$criteria->compare ( 'Start_Date', $this->Start_Date, true );
+		$criteria->compare ( 'End_Date', $this->End_Date, true );
+		$criteria->compare ( 'adminProject', $this->adminProject, true );
+		
+		return new CActiveDataProvider ( $this, array (
+				'criteria' => $criteria 
+		) );
 	}
-
+	
 	/**
 	 * Returns the static model of the specified AR class.
 	 * Please note that you should have this exact method in all your CActiveRecord descendants!
-	 * @param string $className active record class name.
+	 * 
+	 * @param string $className
+	 *        	active record class name.
 	 * @return Project the static model class
 	 */
-	public static function model($className=__CLASS__)
-	{
-		return parent::model($className);
+	public static function model($className = __CLASS__) {
+		return parent::model ( $className );
 	}
 }
--- promanpro/protected/models/User.php
+++ promanpro/protected/models/User.php
@@ -10,85 +10,139 @@
  * @property string $Name
  * @property string $Email
  */
-class User extends CActiveRecord
-{
+class User extends CActiveRecord {
 	/**
+	 *
 	 * @return string the associated database table name
 	 */
-
-
-	 // holds the password confirmation word
-    public $Repassword;
- 
-    //will hold the encrypted password for update actions.
-    public $initialPassword;
-
-    public $old_password;
-
-    public $new_password;
-
-	public function tableName()
-	{
+	
+	// holds the password confirmation word
+	public $Repassword;
+	
+	// will hold the encrypted password for update actions.
+	public $initialPassword;
+	public $old_password;
+	public $new_password;
+	public function tableName() {
 		return 'user';
 	}
-
+	
 	/**
+	 *
 	 * @return array validation rules for model attributes.
 	 */
-	public function rules()
-	{
+	public function rules() {
 		// NOTE: you should only define rules for those attributes that
 		// will receive user inputs.
-		return array(
-                        array('ID,Username,Email','unique'),
-                    array('Username','match','not'=>true,'pattern'=>'/[^a-z0-9A-Z]/'),
-                    array('Email','email'),
-                    //array('Username,Password','min'=>8),
-		//	array('Username, Password, Repassword, Name, Email', 'required'),
-                    array('Username, Name, Email', 'required'),
-                    array('Username, Password, Repassword, Name, Email', 'required', 'on' => 'register'),
-			array('Password', 'compare', 'compareAttribute'=>'Repassword', 'on' => 'register' ),
-			array('Username, Password, Name', 'length', 'max'=>20, 'on' => 'register'),
-			array('old_password, new_password, Repassword', 'required', 'on' => 'changePwd'),
-			array('old_password', 'findPasswords', 'on' => 'changePwd'),
-			array('new_password', 'compare', 'compareAttribute'=>'Repassword', 'on' => 'changePwd' ),
-                    array('Password', 'length', 'min'=>6),
-			array('Email', 'length', 'max'=>30),
-			// The following rule is used by search().
-			// @todo Please remove those attributes that should not be searched.
-			array('ID, Username, Password, Name, Email, isActive', 'safe', 'on'=>'search'),
+		return array (
+				array (
+						'ID,Username,Email',
+						'unique' 
+				),
+				array (
+						'Username',
+						'match',
+						'not' => true,
+						'pattern' => '/[^a-z0-9A-Z]/' 
+				),
+				array (
+						'Email',
+						'email' 
+				),
+				// array('Username,Password','min'=>8),
+				// array('Username, Password, Repassword, Name, Email', 'required'),
+				array (
+						'Username, Name, Email',
+						'required' 
+				),
+				array (
+						'Username, Password, Repassword, Name, Email',
+						'required',
+						'on' => 'register' 
+				),
+				array (
+						'Password',
+						'compare',
+						'compareAttribute' => 'Repassword',
+						'on' => 'register' 
+				),
+				array (
+						'Username, Password, Name',
+						'length',
+						'max' => 20,
+						'on' => 'register' 
+				),
+				array (
+						'old_password, new_password, Repassword',
+						'required',
+						'on' => 'changePwd' 
+				),
+				array (
+						'old_password',
+						'findPasswords',
+						'on' => 'changePwd' 
+				),
+				array (
+						'new_password',
+						'compare',
+						'compareAttribute' => 'Repassword',
+						'on' => 'changePwd' 
+				),
+				array (
+						'Password',
+						'length',
+						'min' => 8,
+						'pattern' => '/[^a-z0-9A-Z]/'
+				),
+				array (
+						'Email',
+						'length',
+						'max' => 30 
+				),
+				array (
+						'Name',
+						'length',
+						'max' => 50,
+						'pattern' => '/[^a-zA-Z]/'
+				),
+				// The following rule is used by search().
+				// @todo Please remove those attributes that should not be searched.
+				array (
+						'ID, Username, Password, Name, Email, isActive',
+						'safe',
+						'on' => 'search' 
+				) 
 		);
 	}
-
+	
 	/**
+	 *
 	 * @return array relational rules.
 	 */
-	public function relations()
-	{
+	public function relations() {
 		// NOTE: you may need to adjust the relation name and the related
 		// class name for the relations automatically generated below.
-		return array(
-		);
+		return array ();
 	}
-
+	
 	/**
+	 *
 	 * @return array customized attribute labels (name=>label)
 	 */
-	public function attributeLabels()
-	{
-		return array(
-			'ID' => 'ID',
-			'Username' => 'Username',
-			'Password' => 'Password',
-			'Repassword' => 'Retype Password',
-			'old_password' => 'Old Password',
-			'new_password' => 'New Password',
-			'Name' => 'Name',
-			'Email' => 'Email',
-                        'isActive' => 'isActive'
+	public function attributeLabels() {
+		return array (
+				'ID' => 'ID',
+				'Username' => 'Username',
+				'Password' => 'Password',
+				'Repassword' => 'Retype Password',
+				'old_password' => 'Old Password',
+				'new_password' => 'New Password',
+				'Name' => 'Name',
+				'Email' => 'Email',
+				'isActive' => 'isActive' 
 		);
 	}
-
+	
 	/**
 	 * Retrieves a list of models based on the current search/filter conditions.
 	 *
@@ -99,52 +153,46 @@ class User extends CActiveRecord
 	 * - Pass data provider to CGridView, CListView or any similar widget.
 	 *
 	 * @return CActiveDataProvider the data provider that can return the models
-	 * based on the search/filter conditions.
+	 *         based on the search/filter conditions.
 	 */
-	public function search()
-	{
+	public function search() {
 		// @todo Please modify the following code to remove attributes that should not be searched.
-
-		$criteria=new CDbCriteria;
-
-		$criteria->compare('ID',$this->ID);
-		$criteria->compare('Username',$this->Username,true);
-		$criteria->compare('Password',$this->Password,true);
-		$criteria->compare('Name',$this->Name,true);
-		$criteria->compare('Email',$this->Email,true);
-                $criteria->compare('isActive',$this->isActive,true);
-
-		return new CActiveDataProvider($this, array(
-			'criteria'=>$criteria,
-		));
+		$criteria = new CDbCriteria ();
+		
+		$criteria->compare ( 'ID', $this->ID );
+		$criteria->compare ( 'Username', $this->Username, true );
+		$criteria->compare ( 'Password', $this->Password, true );
+		$criteria->compare ( 'Name', $this->Name, true );
+		$criteria->compare ( 'Email', $this->Email, true );
+		$criteria->compare ( 'isActive', $this->isActive, true );
+		
+		return new CActiveDataProvider ( $this, array (
+				'criteria' => $criteria 
+		) );
 	}
-
 	public function beforeSave() {
-        $pass = md5($this->Password);
-        $this->Password = $pass;
-        return true;
-    }
-
-    public function findPasswords($attribute, $params)
-    {
-    	$user = User::model()->findByPk(Yii::app()->user->id);
-    	echo 'user'. $user->Password.'</br>';
-    	echo 'old ' . md5($this->old_password).'</br>';
-    	echo 'ID ' . $user->ID;
-    	if($user->Password != md5($this->old_password))
-    		$this->addError($attribute, 'Old password is incorrect.');
-    }
-
-
-
+		$pass = md5 ( $this->Password );
+		$this->Password = $pass;
+		return true;
+	}
+	public function findPasswords($attribute, $params) {
+		$user = User::model ()->findByPk ( Yii::app ()->user->id );
+		echo 'user' . $user->Password . '</br>';
+		echo 'old ' . md5 ( $this->old_password ) . '</br>';
+		echo 'ID ' . $user->ID;
+		if ($user->Password != md5 ( $this->old_password ))
+			$this->addError ( $attribute, 'Old password is incorrect.' );
+	}
+	
 	/**
 	 * Returns the static model of the specified AR class.
 	 * Please note that you should have this exact method in all your CActiveRecord descendants!
-	 * @param string $className active record class name.
+	 * 
+	 * @param string $className
+	 *        	active record class name.
 	 * @return User the static model class
 	 */
-	public static function model($className=__CLASS__)
-	{
-		return parent::model($className);
+	public static function model($className = __CLASS__) {
+		return parent::model ( $className );
 	}
 }
